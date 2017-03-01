c196695afdeef5405b4787d63099109d9d91d062
randomcoww <etherblimp@gmail.com>
2017-02-28 23:17:37 -0800

    test name

--- attributes/default.rb
+++ attributes/default.rb
@@ -1,6 +1,6 @@
 node.default['nftables']['pkg_update_command'] = "apt-get update -qqy"
 node.default['nftables']['pkg_names'] = ['git', 'nftables']
-node.default['nftables']['instances']['ns'] = {
+node.default['nftables']['instances']['test'] = {
   'git_repo' => 'https://github.com/randomcoww/nftables-docker_config.git',
   'git_branch' => 'master',
   'template_variables' => {
