3cc046687c8ae765a3efd33a45fd6a5f7f0f0536
randomcoww <etherblimp@gmail.com>
2017-03-04 16:54:14 -0800

    remove attr

--- attributes/default.rb
+++ attributes/default.rb
@@ -1,10 +1,10 @@
-node.default['nftables']['pkg_update_command'] = "apt-get update -qqy"
-node.default['nftables']['pkg_names'] = ['git', 'nftables']
-node.default['nftables']['instances']['test'] = {
-  'git_repo' => 'https://github.com/randomcoww/nftables-config.git',
-  'git_branch' => 'test',
-  'template_variables' => {
-    'docker_ip1' => '172.20.0.3',
-    'docker_ip2' => '172.20.0.4'
-  }
-}
+# node.default['nftables']['pkg_update_command'] = "apt-get update -qqy"
+# node.default['nftables']['pkg_names'] = ['git', 'nftables']
+# node.default['nftables']['instances']['test'] = {
+#   'git_repo' => 'https://github.com/randomcoww/nftables-config.git',
+#   'git_branch' => 'test',
+#   'template_variables' => {
+#     'docker_ip1' => '172.20.0.3',
+#     'docker_ip2' => '172.20.0.4'
+#   }
+# }
